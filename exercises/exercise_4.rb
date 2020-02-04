8897b69689a64a3e9577c5ccf639291b21083f27
Anisa <anisahamohamed@gmail.com>
2020-02-04 01:14:36 +0000

    loaded a subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,9 +7,17 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
-Surrey (annual_revenue of 224000, carries women's apparel only)
-Whistler (annual_revenue of 1900000 carries men's apparel only)
-Yaletown (annual_revenue of 430000 carries men's and women's apparel)
-Using the where class method from Active Record, fetch (a collection of) only those stores that carry men's apparel. Assign the results to a variable @mens_stores.
-Loop through each of these stores and output their name and annual revenue on each line.
-Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
+Surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+@mens_stores = Store.where(mens_apparel: true)
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue <= :revenue",
+  {:revenue => 1000000})
+@mens_stores.each do |store| puts store.name end
+@womens_stores.each do |store| puts store.name end
+    # Surrey (annual_revenue of 224000, carries women's apparel only)
+# Whistler (annual_revenue of 1900000 carries men's apparel only)
+# Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+# Using the where class method from Active Record, fetch (a collection of) only those stores that carry men's apparel. Assign the results to a variable @mens_stores.
+# Loop through each of these stores and output their name and annual revenue on each line.
+# Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
